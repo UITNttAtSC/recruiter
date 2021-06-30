9b25e5ebcba92a6e8cc439cf77dfd0435d903bb7
Khin Me Me Latt <khinmemelatt@outlook.com>
2021-06-30 20:00:10 +0630

    Remove unnecessary functions

--- src/main/java/com/recruiter/recruiter/controller/CompanyController.java
+++ src/main/java/com/recruiter/recruiter/controller/CompanyController.java
@@ -31,51 +31,7 @@ public class CompanyController {
   
     @Autowired
     UserService userService;
-  
-
-    @RequestMapping("/add")
-  	public String addCompany(Model model) {
-		
-		Company company=new Company();
-		
-		model.addAttribute("company", company);
-		
-		return "addCompany";	
-		
-	}
-	
-	@PostMapping("/add")
-	public String addCompanyPost(@ModelAttribute("company") Company company,
-			HttpServletRequest request,
-			@ModelAttribute("companyLogo") MultipartFile companyLogo,
-            @ModelAttribute("companyFeaturePhoto") MultipartFile companyFeaturePhoto
-			) throws IOException {
-				companyService.save(company);
-
-				byte[] bytes = companyLogo.getBytes();
-
-				String name = "logo"+ company.getCompanyId() + ".png";
-
-				BufferedOutputStream stream = new BufferedOutputStream(
-						new FileOutputStream(new File("src/main/resources/static/image/companylogo/" + name)));
-
-				stream.write(bytes);
-                stream.close();
-
-                byte[] bytes1 = companyFeaturePhoto.getBytes();
-
-				String name1 = "featurePhoto"+ company.getCompanyId() + ".png";
-
-				BufferedOutputStream stream1 = new BufferedOutputStream(
-						new FileOutputStream(new File("src/main/resources/static/image/companyFeaturePhoto/" + name1)));
-
-				stream1.write(bytes1);
-				stream1.close();
-
-				return "redirect:/book/bookList";
-		
-	}
-    
+     
     @RequestMapping("/newCompany")
     private String createCompanyProfile(Model model, Principal principal){
         // User user = userService.findByUsername(principal.getName());
