82805e04997242b48a4f861d90b476e8dcb027bd
ThawMyoHtet <thawmyohtet001@gmail.com>
2021-07-01 22:25:17 +0630

    config /about

--- src/main/java/com/recruiter/recruiter/config/SecurityConfig.java
+++ src/main/java/com/recruiter/recruiter/config/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 				"/fonts/**",
 				"/",
 				"/index",
+				"/about",
 				"/userRegister",
 				"/searchPostWithCategoryAndLocation",
 				"/imags/**",
