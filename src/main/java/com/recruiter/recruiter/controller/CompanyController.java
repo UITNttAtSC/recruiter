977ca7ddfa72924bc2a1706d16fe51ed6670c3bc
KhinMeMeLatt <khinmemelatt@uit.edu.mm>
2021-07-01 15:51:30 +0630

    Company Info edit

--- src/main/java/com/recruiter/recruiter/controller/CompanyController.java
+++ src/main/java/com/recruiter/recruiter/controller/CompanyController.java
@@ -13,24 +13,30 @@ import com.recruiter.recruiter.domain.Payment;
 import com.recruiter.recruiter.domain.User;
 import com.recruiter.recruiter.service.CompanyService;
 import com.recruiter.recruiter.service.UserService;
+import com.recruiter.recruiter.service.impl.UserSecurityService;
+import com.recruiter.recruiter.utility.SecurityUtility;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 @Controller
-@RequestMapping("/companyInfo") 
 public class CompanyController {
    
     @Autowired
     private CompanyService companyService;
+
+    @Autowired
+	private UserSecurityService userSecurityService;
   
   
     @Autowired
@@ -44,7 +50,7 @@ public class CompanyController {
         model.addAttribute("company", company);
         model.addAttribute("user", user);
         model.addAttribute("action", "create");
-        return "company_register";
+        return "company_edit";
     }
 
     @RequestMapping("/companyAccount")
@@ -55,60 +61,83 @@ public class CompanyController {
         model.addAttribute("company", company);
         model.addAttribute("user", user);
         model.addAttribute("action", "update");
-        return "company_register";
+        return "company_edit";
     }
     
-    @PostMapping("/newCompany")
-    public String newCompanyPost(@ModelAttribute("company") Company company,
-    @ModelAttribute("user") User currentUser,
-    @ModelAttribute("password") String password,
-    @ModelAttribute("action") String action,
-    @ModelAttribute("companyLogo") MultipartFile companyLogo,
-    @ModelAttribute("companyFeaturePhotos") List<MultipartFile> companyFeaturePhotos,
-    Model model)
-    throws IOException{
+    @PostMapping(value="/newCompany") 
+    public String newCompany(@ModelAttribute("company") Company company,
+    @ModelAttribute("password") String password,@ModelAttribute("user") User nowUser,
+    @ModelAttribute("newPassword") String newPassword, Model model, Principal principal ) throws Exception { 
         
-        byte[] bytes = companyLogo.getBytes();
-        
-        String name = "logo"+ company.getCompanyName() + ".png";
-        
-        BufferedOutputStream stream = new BufferedOutputStream(
-        new FileOutputStream(new File("src/main/resources/static/image/companyLogo/" + name)));
-        
-        stream.write(bytes);
-        
-        int i = 0;
-        for(MultipartFile companyFeaturePhoto : companyFeaturePhotos){
-            String fileName = "featurePhoto"+ company.getCompanyName() + i + ".png";
-            byte[] bytes1 = companyFeaturePhoto.getBytes();
-            stream = new BufferedOutputStream(
-                new FileOutputStream(new File("src/main/resources/static/image/companyFeaturePhoto/" + fileName)));
-            i++;
-            stream.write(bytes1);
-        }
-        stream.close();
-        
-        if (companyService.findByCompanyName(company.getCompanyName()) != null && action.equals("create")) {
-            model.addAttribute("companyNameExists", true);
-            
-            return "company_register";
-        }
-        
-        User user = userService.findByEmail(currentUser.getEmail());
-        user.setPassword(password);
+    // company.setUser(nowUser);
+    if(nowUser == null) { 
+        throw new Exception ("User not found"); 
+      }
 
-        Company currentCompany = companyService.findByUser_Id(user.getUserId());
-        currentCompany.setCompanyName(company.getCompanyName());
-        currentCompany.setCompanyPhone(company.getCompanyPhone());
-        currentCompany.setCompanyWebsite(company.getCompanyWebsite());
-        currentCompany.setCompanyType(company.getCompanyType());
-        currentCompany.setNoOfEmployee(company.getNoOfEmployee());
-        currentCompany.setCompanyAddress(company.getCompanyAddress());
-        currentCompany.setCompanyMission(company.getCompanyMission());
-        currentCompany.setCompanyVision(company.getCompanyVision());
-        // user.setPassword(SecurityUtility.passwordEncoder().encode(password));
-        companyService.save(user, currentCompany);
-        return "index";
-    }
+      User currentUser = userService.findByUsername(principal.getName());
+    
+     //check email already exists 
+      if (userService.findByEmail(nowUser.getEmail()) !=null) {
+          if(userService.findByEmail(nowUser.getEmail()).getUserId() != currentUser.getUserId()) {
+              model.addAttribute("emailExists", true);
+              return "company_edit";
+          }
+      }
+      
+      /*check username already exists*/
+      if (userService.findByUsername(nowUser.getUsername())!=null){
+          if(userService.findByUsername(nowUser.getUsername()).getUserId() != nowUser.getUserId()) {
+              model.addAttribute("usernameExists", true);
+              return "company_edit";
+          }
+      }
+    
+      //update password
+      if (newPassword != null && !newPassword.isEmpty() && !newPassword.equals("")){
+          BCryptPasswordEncoder passwordEncoder = SecurityUtility.passwordEncoder();
+          String dbPassword = currentUser.getPassword();
+          if(passwordEncoder.matches(password, dbPassword)){
+  
+            currentUser.setPassword(passwordEncoder.encode(newPassword));
+          } else {
+              model.addAttribute("incorrectPassword", true);
+              
+              return "company_edit";
+          }
+      }
+    
+   
+   
+      Company currentCompany = companyService.findByUser_Id(currentUser.getUserId());
+      currentCompany.setCompanyName(company.getCompanyName());
+      currentCompany.setCompanyPhone(company.getCompanyPhone());
+      currentCompany.setCompanyWebsite(company.getCompanyWebsite());
+      currentCompany.setCompanyType(company.getCompanyType());
+      currentCompany.setNoOfEmployee(company.getNoOfEmployee());
+      currentCompany.setCompanyAddress(company.getCompanyAddress());
+      currentCompany.setCompanyMission(company.getCompanyMission());
+      currentCompany.setCompanyVision(company.getCompanyVision());
+      // user.setPassword(SecurityUtility.passwordEncoder().encode(password));
+        
+    //   currentCompany.setUser(nowUser);  
+  //  userService.save(currentUser);
+    companyService.save(currentUser, currentCompany);
+    
+    model.addAttribute("updateSuccess", true);
+    model.addAttribute("classActiveEdit", true);
+    
+    
+    UserDetails userDetails = userSecurityService.loadUserByUsername(nowUser.getUsername());
+    
+    Authentication authentication = new
+    UsernamePasswordAuthenticationToken(userDetails, userDetails.getPassword(),
+    userDetails.getAuthorities());
+    
+    SecurityContextHolder.getContext().setAuthentication(authentication);
+    
+    return "redirect:/";
+    
+   }
+
     
 }
